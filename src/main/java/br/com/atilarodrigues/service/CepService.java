04b84030b3560f38d15baef41d15d78feefcad6d
Átila Rodrigues <atila.rodriguesal@gmail.com>
2024-04-26 18:32:06 -0300

    Add exceptions and findAllAdresses to CepService

--- src/main/java/br/com/atilarodrigues/service/CepService.java
+++ src/main/java/br/com/atilarodrigues/service/CepService.java
@@ -1,15 +1,16 @@
 package br.com.atilarodrigues.service;
 
+import br.com.atilarodrigues.exceptions.CepInvalidExpection;
+import br.com.atilarodrigues.exceptions.CepNotFoundException;
 import br.com.atilarodrigues.model.CepModel;
 import br.com.atilarodrigues.repository.CepRepository;
 import com.google.gson.Gson;
 
-import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 
 public class CepService {
 
@@ -19,21 +20,37 @@ public class CepService {
         cepRepository = new CepRepository();
     }
 
-    public void getCepRequest(String cep) throws URISyntaxException, IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
+    public CepModel saveAddressRequest(String cep) {
+        try {
+            if (cep.length() != 8) {
+                throw new CepInvalidExpection();
+            }
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://brasilapi.com.br/api/cep/v1/" + cep))
-                .version(HttpClient.Version.HTTP_2)
-                .GET()
-                .build();
+            HttpClient client = HttpClient.newHttpClient();
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("https://brasilapi.com.br/api/cep/v1/" + cep))
+                    .version(HttpClient.Version.HTTP_2)
+                    .GET()
+                    .build();
 
-        Gson gson = new Gson();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 404) {
+                throw new CepNotFoundException();
+            }
 
-        cepRepository.saveAddress(gson.fromJson(response.body(), CepModel.class));
+            Gson gson = new Gson();
+            return cepRepository.saveAddress(gson.fromJson(response.body(), CepModel.class));
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
+
+    public ArrayList<CepModel> findAllAddresses() {
+        return cepRepository.findAllAddresses();
+    }
+
 
 }

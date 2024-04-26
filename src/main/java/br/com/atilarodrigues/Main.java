58d906f1123a1756027affb9b99cf0fcddef894c
Átila Rodrigues <atila.rodriguesal@gmail.com>
2024-04-26 00:12:20 -0300

    Make request and store cep in db with main function

--- src/main/java/br/com/atilarodrigues/Main.java
+++ src/main/java/br/com/atilarodrigues/Main.java
@@ -1,7 +1,15 @@
 package br.com.atilarodrigues;
 
+import br.com.atilarodrigues.repository.CepRepository;
+import br.com.atilarodrigues.service.CepService;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+        CepRepository cepRepository = new CepRepository();
+        CepService cepService = new CepService(cepRepository);
+        cepService.getCepRequest("50010020");
     }
 }

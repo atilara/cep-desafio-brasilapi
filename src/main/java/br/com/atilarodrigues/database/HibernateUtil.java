928b332f88ea7b7c7439dfbb61a4daebc58e14f0
Átila Rodrigues <atila.rodriguesal@gmail.com>
2024-04-26 00:08:37 -0300

    Configure hibernate to connect with database

---
+++ src/main/java/br/com/atilarodrigues/database/HibernateUtil.java
@@ -0,0 +1,41 @@
+package br.com.atilarodrigues.database;
+
+import br.com.atilarodrigues.model.CepModel;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            if (sessionFactory == null) {
+
+                Configuration config = new Configuration()
+                        .configure(HibernateUtil.class.getResource("/hibernate.cfg.xml"));
+                config.addAnnotatedClass(CepModel.class);
+                StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
+                serviceRegistryBuilder.applySettings(config.getProperties());
+                ServiceRegistry serviceRegistry = serviceRegistryBuilder.build();
+                sessionFactory = config.buildSessionFactory(serviceRegistry);
+
+            }
+
+            return sessionFactory;
+        } catch (Throwable ex) {
+            System.err.println("Criação da Sessão falhou." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
